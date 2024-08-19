566407dcb92ddcd30799bfd777555c486185250e
Andy Lo <andylolu24@gmail.com>
2024-08-19 23:01:31 +0800

    Visualise marginalised flow

--- flow_matching/dataset/toy.py
+++ flow_matching/dataset/toy.py
@@ -5,22 +5,33 @@ from flow_matching.dataset.base import Dataset
 
 class ToyDataset(Dataset):
     def __init__(self):
-        self.means = np.array([[4.0, 0.0], [-4.0, 0.0]])
-        self.covariances = np.array([
-            [[1.0, 0.7], [0.7, 1.0]], 
-            [[1.0, 0.7], [0.7, 1.0]], 
-        ])
+        self.means = np.array([[4.0, -2.0], [-4.0, 3.0]])
+        self.covariances = np.array(
+            [
+                [[1.0, 0.7], [0.7, 1.0]],
+                [[1.0, 0.7], [0.7, 1.0]],
+            ]
+        )
+
+        # self.means = np.array([[4.0, -2.0]])
+        # self.covariances = np.array(
+        #     [
+        #         [[1.0, 0.7], [0.7, 1.0]],
+        #     ]
+        # )
 
     def dimensions(self) -> tuple[int, ...]:
         return (2,)
 
     def sample(self, batch_size: int) -> np.ndarray:
         # return np.random.multivariate_normal(self.mean, self.covariance, batch_size)
-        samples = np.array([
-            np.random.multivariate_normal(mean, covariance, batch_size)
-            for mean, covariance in zip(self.means, self.covariances)
-        ])
-        idx = np.random.randint(0, 2, batch_size)
+        samples = np.array(
+            [
+                np.random.multivariate_normal(mean, covariance, batch_size)
+                for mean, covariance in zip(self.means, self.covariances)
+            ]
+        )
+        idx = np.random.randint(0, len(self.means), batch_size)
         return samples[idx, np.arange(batch_size)]
 
 
--- flow_matching/field/gaussian.py
+++ flow_matching/field/gaussian.py
@@ -1,23 +1,29 @@
 import jax
 import jax.numpy as jnp
 
-def _u(x, t, x1, mu, sigma):
+
+def mu(x, t):
+    return t * x
+
+
+def sigma(x, t):
+    return 1 - (1 - 1e-5) * t
+
+
+def p(x, t, x1):
+    """p_t(x | x_1) = N(x | mu(x_1, t), sigma(x_1, t))"""
+    return jax.scipy.stats.multivariate_normal.pdf(
+        x, mu(x1, t), sigma(x1, t) ** 2 * jnp.eye(x.shape[-1])
+    )
+
+
+def u(x, t, x1):
     """u_t(x | x_1) = sigma'_t(x_1) / sigma_t(x_1) (x - mu_t(x_1)) + mu'_t(x_1)
-    
+
     mu: (x, t) -> R^d
     sigma: (x, t) -> R
-
-    -(1 - 1e-5) / (1 - (1 - 1e-5) * t) * (x - t * x_1) + x_1
-    = -(1 - 1e-5)
     """
     dmu_dt = jax.jacfwd(mu, argnums=1)
     dsigma_dt = jax.jacfwd(sigma, argnums=1)
-    
+
     return (dsigma_dt(x1, t) / sigma(x1, t)) * (x - mu(x1, t)) + dmu_dt(x1, t)
-
-def u_ot(x, t, x1):
-    # mu = lambda x, t: t * x1
-    # sigma = lambda x, t: 1 - (1 - 1e-5) * t
-    # return _u(x, t, x1, mu, sigma)
-
-    return (x1 - (1 - 1e-5)  *x) / (1 - (1 - 1e-5) * t)
