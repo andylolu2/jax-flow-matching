208e43850876b73cc197aaaea82cb42172ca0543
Andy Lo <andylolu24@gmail.com>
2024-09-24 01:22:01 +0200

    Refactor, might have broke things

--- flow_matching/model/base.py
+++ flow_matching/model/base.py
@@ -17,19 +17,17 @@ class ModelMetrics(Collection):
 
 class Model(nn.Module):
     def __call__(
-        self, x1: Float[ArrayLike, "batch *dims"], rng: PRNGKeyArray, train: bool
+        self, x1: Float[ArrayLike, "*dims"], rng: PRNGKeyArray, train: bool
     ) -> tuple[Loss, ModelMetrics]:
-        batch = jnp.shape(x1)[0]
+        # batch = jnp.shape(x1)[0]
         t_rng, x_rng, fwd_rng = jax.random.split(rng, 3)
 
-        t = jax.random.uniform(t_rng, (batch,))
-        x = jax.vmap(gaussian_flow.sample)(t, x1, jax.random.split(x_rng, batch))
+        t = jax.random.uniform(t_rng)
+        x = gaussian_flow.sample(t, x1, x_rng)
 
-        u_target = jax.vmap(gaussian_flow.u)(x, t, x1)
-        u_pred = jax.vmap(self.forward, in_axes=(0, 0, None, 0))(
-            x, t, train, jax.random.split(fwd_rng, batch)
-        )
-        loss = jnp.mean(jnp.sum((u_pred - u_target) ** 2, axis=-1))
+        u_target = gaussian_flow.u(x, t, x1)
+        u_pred = self.forward(x, t, train, fwd_rng)
+        loss = jnp.mean(jnp.sum((u_pred - u_target) ** 2))
         return loss, ModelMetrics.single_from_model_output(loss=loss)
 
     def forward(
--- flow_matching/train.py
+++ flow_matching/train.py
@@ -1,34 +1,34 @@
 import math
+import tempfile
 from functools import partial
-from os import PathLike
 from pathlib import Path
-from typing import Any, Callable
+from typing import Any
 
-import flax.struct
+import fsspec
+import fsspec.generic
 import jax
 import jax.numpy as jnp
 import matplotlib.pyplot as plt
 import numpy as np
-import optax
 import orbax.checkpoint
 import wandb
 from absl import app, flags, logging
 from clu import metric_writers
-from clu.metrics import Collection, LastValue
 from diffrax import Dopri5, ODETerm, diffeqsolve
-from flax.training.train_state import TrainState as _TrainState
-from jaxtyping import Array, Float, PRNGKeyArray
+from jaxtyping import Array, Float
 from matplotlib.backends.backend_agg import FigureCanvasAgg
 from ml_collections import FrozenConfigDict, config_flags
 
+from flow_matching.builder import (
+    TrainMetrics,
+    TrainState,
+    build_dataset,
+    build_model,
+    build_optimizer,
+    build_train_state,
+)
 from flow_matching.dataset.base import Dataset
-from flow_matching.dataset.cifar10 import Cifar10Dataset
-from flow_matching.dataset.mnist import MnistDataset
-from flow_matching.dataset.toy import ToyDataset
-from flow_matching.model.base import Model, ModelMetrics
-from flow_matching.model.cnn import CNN
-from flow_matching.model.mlp import MLP
-from flow_matching.model.unet import UNet
+from flow_matching.model.base import ModelMetrics
 from flow_matching.third_party.clu.wandb_writer import WandbWriter
 
 FLAGS = flags.FLAGS
@@ -37,66 +37,13 @@ config_flags.DEFINE_config_file(
 )
 
 
-@flax.struct.dataclass
-class TrainMetrics(Collection):
-    step: LastValue.from_output("step")  # type: ignore
-    epoch: LastValue.from_output("epoch")  # type: ignore
-
-
-class TrainState(_TrainState):
-    forward_fn: Callable = flax.struct.field(pytree_node=False)
-    rng: PRNGKeyArray
-    train_metrics: TrainMetrics
-    model_metrics: ModelMetrics
-    train_dataset: Dataset
-    val_dataset: Dataset
-
-
-def build_dataset(name: str, **kwargs) -> tuple[Dataset, Dataset]:
-    if name == "toy":
-        return (
-            ToyDataset.create(**kwargs),
-            ToyDataset.create(**kwargs),
-        )
-    elif name == "mnist":
-        return (
-            MnistDataset.create(**kwargs, split="train"),
-            MnistDataset.create(**kwargs, split="val"),
-        )
-    elif name == "cifar10":
-        return (
-            Cifar10Dataset.create(**kwargs, split="train"),
-            Cifar10Dataset.create(**kwargs, split="val"),
-        )
-
-    raise ValueError(f"Unknown dataset: {name}")
-
-
-def build_model(name: str, **kwargs) -> Model:
-    if name == "mlp":
-        return MLP(**kwargs)
-    elif name == "cnn":
-        return CNN(**kwargs)
-    elif name == "unet":
-        return UNet(**kwargs)
-
-    raise ValueError(f"Unknown model: {name}")
-
-
-def build_optimizer(name: str, **kwargs) -> optax.GradientTransformation:
-    if name == "adam":
-        return optax.adam(**kwargs)
-
-    raise ValueError(f"Unknown optimizer: {name}")
-
-
 @partial(jax.jit, static_argnames=("config",))
 def train_step(train_state: TrainState, config) -> TrainState:
     x, train_dataset = train_state.train_dataset.sample(config.batch_size)
     rng, fwd_rng = jax.random.split(train_state.rng)
-    grads, new_model_metrics = jax.grad(train_state.apply_fn, has_aux=True)(
-        train_state.params, x, fwd_rng, train=True
-    )
+    grads, new_model_metrics = jax.grad(
+        jax.vmap(train_state.apply_fn, in_axes=(None, 0, 0, None)), has_aux=True
+    )(train_state.params, x, jax.random.split(fwd_rng, config.batch_size), train=True)
     train_state = train_state.apply_gradients(grads=grads)
     model_metrics = train_state.model_metrics.merge(new_model_metrics)
     new_train_metrics = TrainMetrics.single_from_model_output(
@@ -119,11 +66,19 @@ def log_metrics(train_state: TrainState, writer: metric_writers.MetricWriter) ->
         metric_writers.write_values(writer, step=int(train_state.step), metrics=values)
 
 
-def save_checkpoint(train_state: TrainState, path: PathLike) -> None:
+def save_checkpoint(train_state: TrainState, path: str) -> None:
     logging.info("Saving checkpoint to %s", path)
-    checkpointer = orbax.checkpoint.StandardCheckpointer()
-    checkpointer.save(Path(path).absolute().resolve(), train_state)
-    checkpointer.wait_until_finished()
+
+    # save to temporary directory, then copy to final destination
+    with tempfile.TemporaryDirectory() as tmpdir:
+        tmp_path = Path(tmpdir)
+        checkpointer = orbax.checkpoint.StandardCheckpointer()
+        checkpointer.save(tmp_path, train_state)
+        checkpointer.wait_until_finished()
+
+        fs = fsspec.generic.GenericFileSystem()
+        fs.put(tmp_path, path, recursive=True)
+        fs.copy
 
 
 @partial(jax.jit, static_argnames=("batch_size",))
@@ -164,7 +119,6 @@ def evaluate(
     )
 
 
-@partial(jax.jit, static_argnames=("n",))
 def _generate_samples(train_state: TrainState, n: int) -> Float[Array, "{n} ..."]:
     _x, _ = train_state.train_dataset.sample(1)
     size = math.prod(jnp.shape(_x)[1:])
@@ -254,18 +208,8 @@ def main(_):
     train_dataset, val_dataset = build_dataset(**config.dataset)
     model = build_model(**config.model)
     optimizer = build_optimizer(**config.optimizer)
-
-    params = model.init(model_rng, train_dataset.sample(1)[0], fwd_rng, train=True)
-    train_state = TrainState.create(
-        apply_fn=model.apply,
-        forward_fn=model.forward,
-        params=params,
-        tx=optimizer,
-        train_metrics=TrainMetrics.empty(),
-        model_metrics=ModelMetrics.empty(),
-        train_dataset=train_dataset,
-        val_dataset=val_dataset,
-        rng=state_rng,
+    train_state = build_train_state(
+        model, optimizer, train_dataset, val_dataset, state_rng
     )
 
     while train_state.step < config.num_steps:
@@ -278,7 +222,7 @@ def main(_):
                 train_metrics=TrainMetrics.empty(), model_metrics=ModelMetrics.empty()
             )
         if config.save.steps > 0 and step % config.save.steps == 0:
-            save_checkpoint(train_state, Path(config.checkpoint_dir) / f"step_{step}")
+            save_checkpoint(train_state, f"{config.checkpointj_dir}/step_{step}/")
         if config.eval.steps > 0 and step % config.eval.steps == 0:
             evaluate(
                 train_state,
